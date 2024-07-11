4de161df6394d58fa3c2489ad2f1ae4645b282be
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-11 14:56:02 -0600

    set sitewide css for layout and add toggle function for sound to accessibility component

--- stoop-sale/src/App.js
+++ stoop-sale/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Accessibility from './components/Accessibility';
 import Details from './components/Details';
 import Footer from './components/Footer';
@@ -9,9 +9,19 @@ import Hero from './components/Hero';
 
 
 function App() {
+  const [soundOn, setSoundOn] = useState(true);
+
+  function toggleSound() {
+    setSoundOn(!soundOn);
+  }
+
+
   return (
     <div className="App">
-      <Accessibility />
+      <Accessibility 
+      toggleSound={toggleSound}
+      soundOn={soundOn}
+      />
       <Hero />
       <Details /> {/* date, time, map => Angel */}
       <Share /> {/* share icons => Angel */}
--- stoop-sale/src/components/Accessibility.js
+++ stoop-sale/src/components/Accessibility.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Accessibility.css';
+import speakerOn from './images/mute_icon.png';
+import speakerOff from './images/speaker icon ON.png';
 
-export default function Accessibility() {
+export default function Accessibility(props) {
+    
+    
     return (
         <div>
-            <p>Turn OFF Sound</p>
+            <img src={props.soundOn === true ? speakerOn : speakerOff} onClick={props.toggleSound}/><p>Turn OFF Sound</p>
             <p>Turn OFF Animation</p>
         </div>
     )
