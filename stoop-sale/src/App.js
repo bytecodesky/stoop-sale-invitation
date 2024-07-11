d5ce3c5a69efdfa38e3f351a288c32c9f64b8840
bytecodesky <skynetoffc@gmail.com>
2024-07-11 17:24:20 -0500

    chore: Remove unused Map component and add social share icons to Share component

--- stoop-sale/src/App.js
+++ stoop-sale/src/App.js
@@ -6,8 +6,6 @@ import Footer from './components/Footer';
 import Gallery from './components/Gallery';
 import Share from './components/Share';
 import Hero from './components/Hero';
-import Map from './components/Map';
-
 
 function App() {
   const [soundOn, setSoundOn] = useState(true);
@@ -34,17 +32,17 @@ function App() {
       <Details />
       <div>
         {/* date, time, map => Angel */}
-        <p>Date: July 25, 2024</p>
-        <p>Time: 10:00 AM - 4:00 PM</p>
-        <p>Location: 2 PL, Court Street, Brooklyn, New York</p>
-      </div> 
-      <Share />
-      <Map />
-       {/* share icons => Angel */}
+        <div>
+          <p>Date: July 25, 2024</p>
+          <p>Time: 10:00 AM - 4:00 PM</p>
+          <p>Location: 2 PL, Court Street, Brooklyn, New York</p>
+        </div>
+        </div>
+        <Share />
+        {/* share icons => Angel */}
       <Gallery />
       <Footer />
     </div>
   );
 }
-
 export default App;
--- stoop-sale/src/components/Share.js
+++ stoop-sale/src/components/Share.js
@@ -4,7 +4,18 @@ import './Share.css';
 export default function Share() {
     return (
         <div>
-
+        {/* social share icons => Angel */}
+        <div>
+          <a href="https://www.facebook.com/share?url=https://example.com" target="_blank" rel="noopener noreferrer">
+            <img src="./images/facebook.png" alt="Facebook" />
+          </a>
+          <a href="https://x.com/intent/tweet?url=https://example.com&text=Check%20out%20this%20awesome%20event!" target="_blank" rel="noopener noreferrer">
+            <img src="./images/x.png" alt="Twitter" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src="./images/instagram.png" alt="LinkedIn" />
+          </a>
         </div>
+      </div>
     )
 }
