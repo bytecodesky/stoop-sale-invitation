349ef17840e3a369ecd9f80370cc844c54d1a13d
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-12 07:38:25 -0600

    incorporate teammate changes and remove unneeded files

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,10 +3,10 @@ import './Details.css';
 
 export default function Details() {
     return (
-        <div>            
+        <div className='details-container'>            
             <p>Date: July 25, 2024</p>
             <p>Time: 10:00 AM - 4:00 PM</p>
-            <p>Location: 2 PL, Court Street, Brooklyn, New York</p>     
+            <p>Location: 2 PL & Court Street, Brooklyn, New York</p>     
         </div>
     )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Hero.css';
 
-export default function Hero() {
+export default function Hero(props) {
     return (
         <div>
-            <h1>You're Invited!</h1>
+            <h1 className='hero-title'>You're Invited!</h1>
         </div>
     )
 }
