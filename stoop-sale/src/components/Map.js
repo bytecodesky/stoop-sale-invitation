5e5ae9bccea4d3caf95bd18069b7e03fe4bdbace
bytecodesky <skynetoffc@gmail.com>
2024-07-11 19:22:13 -0500

    chore: Add CSS animation to Map component

--- stoop-sale/src/components/Map.js
+++ stoop-sale/src/components/Map.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './Map.css';
 
 export default function Map() {
     
     return (
-        <div>
-            <iframe title="Google Maps" style={{width:"500", height:"300", display: 'block', margin: '0 0 0 30%'}} loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:EioybmQgUGwgJiBDb3VydCBTdCwgQnJvb2tseW4sIE5ZIDExMjMxLCBVU0EiZiJkChQKEglXGRhcWFrCiRGyde_BuJTmUBIUChIJVxkYXFhawokRsnXvwbiU5lAaFAoSCVuuBfJYWsKJEQIicj09qGFVGhQKEgnhT8evWVrCiRFy1_bXvC3EPSIKDVQzPxgVOOnk0w&key=AIzaSyADaoH9FEmhNoC8x6rTJjRToDihABF-sFk"></iframe>
+        <div class="map">
+            <iframe title="Google Maps" style={{width:"500", height:"300", display: 'block', margin: '0 50% 0 0'}} loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:EioybmQgUGwgJiBDb3VydCBTdCwgQnJvb2tseW4sIE5ZIDExMjMxLCBVU0EiZiJkChQKEglXGRhcWFrCiRGyde_BuJTmUBIUChIJVxkYXFhawokRsnXvwbiU5lAaFAoSCVuuBfJYWsKJEQIicj09qGFVGhQKEgnhT8evWVrCiRFy1_bXvC3EPSIKDVQzPxgVOOnk0w&key=AIzaSyADaoH9FEmhNoC8x6rTJjRToDihABF-sFk"></iframe>
         </div>
     )
 }
